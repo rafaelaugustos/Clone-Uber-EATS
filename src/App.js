24f9ee48bcb41101a4d1e3993c140455811bfa71
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2019-09-09 17:02:27 -0300

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+    return(
+        <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#f5f5f5', alignItems: 'center' }}>
+            <Text>Welcome, Uber EATS</Text>
+        </View>
+    )
+}
+
+export default App
